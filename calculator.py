81f935e40d3d9dd389323384e75b71957aa9a7bc
Joel Rhode <joelrhode@gmail.com>
2020-05-03 10:32:00 -0500

    completed calculator assignment

--- calculator.py
+++ calculator.py
@@ -40,18 +40,72 @@ To submit your homework:
 
 
 """
-
+import traceback
 
 def add(*args):
-    """ Returns a STRING with the sum of the arguments """
+    """Returns sum of provided args"""
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+    try:
+        sum = 0
+        for arg in args:
+            sum += int(arg)
+    except (ValueError, TypeError):
+        return 'Invalid Operands'
 
-    return sum
+    return str(sum)
 
-# TODO: Add functions for handling more arithmetic operations.
+
+def subtract(*args):
+    """Returns difference of provided args"""
+
+    try:
+        diff = int(args[0])
+        for arg in args[1:]:
+            diff -= int(arg)
+    except (ValueError, TypeError):
+        return 'Invalid Operands'
+
+    return str(diff)
+
+
+def multiply(*args):
+    """Returns product of provided args"""
+    
+    try:
+        product = int(args[0])
+        for arg in args[1:]:
+            product *= int(arg)
+    except (ValueError, TypeError):
+        return 'Invalid Operands'
+
+    return str(product)
+
+
+def divide(*args):
+    """Returns quotient of provided args"""
+    
+    try:
+        quotient = int(args[0])
+        for arg in args[1:]:
+            quotient /= int(arg)
+    except (ValueError, TypeError):
+        return 'Invalid Operands'
+    except ZeroDivisionError:
+        return 'Cannot Divide By Zero'
+        
+    return str(quotient)
+
+
+def instructions(*args):
+    """Instructions for calculator"""
+    body = (
+        'Navigate to localhost:8080 to use calculator, followed by slash and',
+        'then operation (add, subtract, multiply, divide). Operands follow,',
+        'seperated by slashes. Operands must be intergers. Result will be displayed.',
+        'Ex: localhost:8080/add/8/9.'
+    )
+        
+    return '\n'.join(body)
 
 def resolve_path(path):
     """
@@ -59,26 +113,48 @@ def resolve_path(path):
     arguments.
     """
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
-
+    funcs = {
+        'add': add,
+        'subtract': subtract,
+        'multiply': multiply,
+        'divide': divide,
+        '': instructions 
+    }
+    path = path.strip('/').split('/')
+    func_name = path[0]
+    args = path[1:]
+    
+    try:
+        func = funcs[func_name]
+    except:
+        raise NameError
+        
     return func, args
 
+
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    headers = [('Content-type', 'text/html')]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = '200 OK'
+    except NameError:
+        status = '404 Not Found'
+        body = '<h1>Not Found</h1>'
+    except Exception:
+        status = '500 Internal Server Error'
+        body = '<h1>Internal Service Error</h1>'
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
